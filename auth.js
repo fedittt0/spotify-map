85da90ee6fbb27e8eebdf3d6d4fd4da5a2239329
fedittto <123590954+fedittt0@users.noreply.github.com>
2025-06-15 19:33:32 -0300

    Add files via upload

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ const scopes = 'user-top-read';
 
 document.getElementById('login-btn').addEventListener('click', () => {
   // ¡¡¡AQUÍ ESTÁ EL CAMBIO IMPORTANTE!!!
-  window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes}&show_dialog=true`;
+  window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes}&show_dialog=true`;
 });
 
 // Obtener el token
